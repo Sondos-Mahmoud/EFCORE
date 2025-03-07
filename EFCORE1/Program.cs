f212f0384a08c2b830e1c6fece0772194c05e792
Sondos-Mahmoud <sondos.mohamed02@eng-st.cu.edu.eg>
2025-03-08 00:12:31 +0200

    final

--- EFCORE1/Program.cs
+++ EFCORE1/Program.cs
@@ -10,24 +10,51 @@ namespace EFCORE1
         {
             using CompanyDbContext dbContext = new CompanyDbContext();
 
-     
 
-            Course c01 = new Course()
+
+            //CRUD OPERATIONS
+            #region Insert
+            //Course c01 = new Course()
+            //{
+            //    Name = "C#",
+            //    Description = "ay haga",
+            //    Duration = "2m"
+            //};
+            //dbContext.Course.Add(c01);
+            //dbContext.Entry(c01).State = EntityState.Added;
+            //var student = new Student
+            //{
+            //    F_Name = "John",
+            //    L_Name = "Doe",
+            //    Address = "123 Main St",
+            //    Age = 25
+            //};
+            //dbContext.Student.Add(student);
+            //dbContext.Entry(student).State = EntityState.Added; 
+
+            #endregion
+            #region retrive
+            var c = dbContext.Course.AsNoTracking().Where(c => c.Name == "C#").FirstOrDefault();
+            //Console.WriteLine(c.Name??"null"); 
+            #endregion
+            #region modify
+            if (c != null)
             {
-                Name = "C#",
-                Description ="ay haga",
-                Duration="2m"
-            };
-            dbContext.Course.Add(c01);
-            dbContext.Entry(c01).State = EntityState.Added;
-         
-            var student = new Student
-            {
-                F_Name = "John",
-                L_Name = "Doe",
-                Address = "123 Main St",
-                Age = 25
-            };
+                c.Name = "java";
+            }
+            #endregion
+            #region deleted
+            //if (c != null)
+            //{
+            //    dbContext.Course.Remove(c);
+            //} 
+            #endregion
+
+
+
+
+
+
             dbContext.SaveChanges();
         }
     }
